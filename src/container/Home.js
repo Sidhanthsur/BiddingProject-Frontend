0c6c84ce86bae8f79012dd59b3e4fd7b73ab3e40
Sidhanth <sidhanth.surana12@gmail.com>
2019-05-10 21:24:37 +0530

    added base navbar

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,11 +1,36 @@
-import React from 'react'
-import { Container } from 'reactstrap'
+import React, { useState } from 'react'
+import { Container, Navbar, NavbarBrand, NavItem, NavLink, Nav, Collapse, NavbarToggler } from 'reactstrap'
 import './styles/HomeStyles.css'
 
+
 export default function Home() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    function toggle() {
+        console.log('yo')
+        setIsOpen(!isOpen)
+    }
     return (
-        <Container fluid className='home-main'>
-            <h1>Home</h1>
-        </Container>
+        <div className='home-div'>
+            <Navbar color='dark' expand="md">
+                <NavbarBrand href="/">Refresh</NavbarBrand>
+                <NavbarToggler onClick={toggle} type='button' >
+                    <i class="fas fa-bars"></i>
+                </NavbarToggler>
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink href='/home'>
+                                Home
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+            <Container fluid className='home-main'>
+
+                <h1>Home</h1>
+            </Container>
+        </div>
     )
 }
